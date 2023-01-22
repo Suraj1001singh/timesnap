6f5521eff99f28d46c518dc750a07b1502d3a20b
Akanksha Kumari <54508862+akanksha00@users.noreply.github.com>
2023-01-22 22:06:49 +0530

    background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,7 @@ export default App;*/
 
 import React, { useState } from "react";
 import "./App.css";
+import Particles from "react-tsparticles";
 import axios from "axios";
 export default function App() {
   const [url, setUrl] = useState("");
@@ -55,9 +56,148 @@ export default function App() {
     }
   }
   return (
-    <div className="App">
+    <>
+    <Particles
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: -1
+        },
+        particles: {
+          number: {
+            value: 200,
+            limit: 300,
+            density: {
+              enable: true,
+              value_area: 800
+            }
+          },
+          color: {
+            value: "#ffffff"
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "images/github.svg",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.5,
+              sync: false
+            }
+          },
+          size: {
+            value: 30,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 10,
+              size_min: 10,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: true,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 1,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble",
+              parallax: {
+                enable: false,
+                force: 60,
+                smooth: 10
+              }
+            },
+            onClick: {
+              enable: true,
+              mode: "push"
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              lineLinked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 400,
+              size: 100,
+              duration: 2,
+              opacity: 1,
+              speed: 2
+            },
+            repulse: {
+              distance: 200
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        backgroundMask: {
+          enable: true,
+          cover: {
+            color: {
+              value: {
+                r: 0,
+                g: 0,
+                b: 0
+              }
+            }
+          }
+        },
+        retina_detect: true,
+        fps_limit: 60,
+        background: {
+          image: "url('https://particles.js.org/images/background3.jpg')"
+        }
+      }}
+    />
+    <div className="App" >
+      {/* <ParticlesBackground/> */}
       <h1>Time Stamp Generator</h1>
-      <form onSubmit={handleSubmit}>
+      <form  onSubmit={handleSubmit}>
         <div className="form-control">
           {/* <label>Enter youtube link</label> */}
           <input
@@ -73,5 +213,8 @@ export default function App() {
         </div>
       </form>
     </div>
+    </>
   );
 }
+
+ 
