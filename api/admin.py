5dd95a7188253fbb0982322d585a924956c89c0c
saksham28hans <92790513+saksham28hans@users.noreply.github.com>
2023-01-27 02:49:43 +0530

    Backend complete

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Student
+from .models import Video,Audio
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ["id","stuname","email"]
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ["id","id_video","output"]
+
+@admin.register(Audio)
+class AudioAdmin(admin.ModelAdmin):
+    list_display = ["id","id_video","output"]
